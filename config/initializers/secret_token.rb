a6cb7d80065bc0331a04b6c3b7e65e9f4e7b8557
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-07-11 13:55:49 -0400

    foo baller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FooTest::Application.config.secret_token = 'f56c390115ae607f96f885f22aa75258f2c7eab45c2963c26aad8666b9a68f8612647ee70e8cfca16ddcfe564ff25d36778589f791d0b33f6b18db797ac4e85a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FooTest::Application.config.session_store :cookie_store, key: '_foo_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FooTest::Application.config.session_store :active_record_store
